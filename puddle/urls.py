79c17dc470f54fb7b954fbdc4db89cfe80a34cc0
vivek <vivekwarkade000@gmail.com>
2023-03-22 13:32:23 +0530

    signup feature

--- puddle/urls.py
+++ puddle/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
-from core.views import index,contact
 
 urlpatterns = [
-    path("",index,name='home'),
-    path("contact/",contact,name='contact'),
+    path("",include('core.urls')),
     path("items/",include('item.urls')),
     path("admin/", admin.site.urls),
 
