b23754732be5753f0f099c7bb52dc285baabd3cf
vivek <vivekwarkade000@gmail.com>
2023-03-22 16:19:49 +0530

    Add login

--- core/views.py
+++ core/views.py
@@ -15,11 +15,14 @@ def contact(request):
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
+
         if form.is_valid():
             form.save()
+
             return redirect('/login/')
     else:        
         form = SignupForm()
+        
     return render(request,'core/signup.html', {
         'form':form
     })
